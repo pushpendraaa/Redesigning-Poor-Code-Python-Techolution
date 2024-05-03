554465abd3df04e9072710ae36aeb0e5965b123b
Pushpendra Kushwaha <89564501+pushpendraaa@users.noreply.github.com>
2024-05-03 21:16:03 +0530

    Delete Redesigning Poor Code - Assignment directory

--- Redesigning Poor Code - Assignment/user.py
+++
@@ -1,8 +0,0 @@
-
-class User:
-    def __init__(self, name,user_id):
-        self.name = name
-        self.user_id = user_id
-
-    def __str__(self):
-        return f"User: {self.name}, ID: {self.user_id}"
